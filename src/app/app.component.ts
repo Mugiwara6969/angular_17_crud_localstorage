139948b5d79cfc325748187f279816ae0d62bf2c
Mugiwara6969 <160160688+Mugiwara6969@users.noreply.github.com>
2024-03-22 04:15:37 +0530

    A crud application using Angular
    it has the ability to recommend medicine according to the disease.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,29 @@
-import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+const XLSX = require('xlsx');
+const readlineSync = require('readline-sync');
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   @ViewChild('myModal') model: ElementRef | undefined;
-  studentObj: Student = new Student();
-  studentList: Student[] = [];
+  patientObj: Patient = new Patient();
+  patientList: Patient[] = [];
 
   ngOnInit(): void {
     const localData = localStorage.getItem("angular17crud");
     if(localData != null) {
-      this.studentList = JSON.parse(localData)
+      this.patientList = JSON.parse(localData)
     }
   }
 
   openModel() {
-    
     const model = document.getElementById("myModal");
     if (model != null) {
       model.style.display = 'block'
@@ -31,76 +31,96 @@ export class AppComponent implements OnInit {
   }
 
   closeModel() {
-    this.studentObj = new Student();
+    this.patientObj = new Patient();
     if (this.model != null) {
       this.model.nativeElement.style.display = 'none';
     }
   }
 
-  onDelete(item: Student) {
+  onDelete(item: Patient) {
     const isDelet = confirm("Are you sure want to Delete");
     if(isDelet) {
-      const currentRecord =  this.studentList.findIndex(m=> m.id === this.studentObj.id);
-      this.studentList.splice(currentRecord,1);
-      localStorage.setItem('angular17crud', JSON.stringify(this.studentList));
+      const currentRecord =  this.patientList.findIndex(m=> m.id === this.patientObj.id);
+      this.patientList.splice(currentRecord,1);
+      localStorage.setItem('angular17crud', JSON.stringify(this.patientList));
     }
   }
-  onEdit(item: Student) {
-    this.studentObj =  item;
+
+  onEdit(item: Patient) {
+    this.patientObj =  item;
     this.openModel();
   }
 
-  updateStud() {
-      const currentRecord =  this.studentList.find(m=> m.id === this.studentObj.id);
-      if(currentRecord != undefined) {
-        currentRecord.name = this.studentObj.name;
-        currentRecord.address =  this.studentObj.address;
-        currentRecord.mobileNo =  this.studentObj.mobileNo;
-      };
-      localStorage.setItem('angular17crud', JSON.stringify(this.studentList));
-      this.closeModel()
+  updatePatient() {
+    const currentRecord =  this.patientList.find(m=> m.id === this.patientObj.id);
+    if(currentRecord != undefined) {
+      currentRecord.name = this.patientObj.name;
+      currentRecord.address =  this.patientObj.address;
+      currentRecord.mobileNo =  this.patientObj.mobileNo;
+    };
+    localStorage.setItem('angular17crud', JSON.stringify(this.patientList));
+    this.closeModel()
   }
-  saveStudent() {
+
+  onSearchValue(): void {
+    const workbook = XLSX.readFile('data01.xlsx');
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+/*     const userInput = readlineSync.question('Enter the value to search: ');
+ */    const userInput = readlineSync.question(this.Sex);
+
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    const result = data.find((row: any) => row['B'] === userInput);
+
+    if (result) {
+      console.log(`Value found: ${result['C']}`);
+    } else {
+        console.log('Value not found.');
+    }
+  }
+
+  savePatient() {
     debugger;
     const isLocalPresent = localStorage.getItem("angular17crud");
     if (isLocalPresent != null) {
       
       const oldArray = JSON.parse(isLocalPresent);
-      this.studentObj.id = oldArray.length + 1;
-      oldArray.push(this.studentObj);
-      this.studentList = oldArray;
+      this.patientObj.id = oldArray.length + 1;
+      oldArray.push(this.patientObj);
+      this.patientList = oldArray;
       localStorage.setItem('angular17crud', JSON.stringify(oldArray));
     } else {
       const newArr = [];
-      newArr.push(this.studentObj);
-      this.studentObj.id = 1;
-      this.studentList = newArr;
+      newArr.push(this.patientObj);
+      this.patientObj.id = 1;
+      this.patientList = newArr;
       localStorage.setItem('angular17crud', JSON.stringify(newArr));
     }
     this.closeModel()
   }
 }
 
-
-export class Student {
+export class Patient {
   id: number;
   name: string;
   mobileNo: string;
   email: string;
-  city: string;
-  state: string;
-  pincode: string;
+  Sex: string;
+  medicine: string;
+  disease: string;
   address: string;
 
   constructor() {
     this.id = 0;
     this.address = '';
-    this.city = '';
+    this.Sex = '';
     this.email = '';
     this.mobileNo = '';
     this.name = '';
-    this.state = '';
-    this.pincode = '';
+    this.medicine = '';
+    this.disease = '';
   }
 
 }
